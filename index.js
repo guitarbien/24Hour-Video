ff98d5e2a5b259eb9556f3e725c16ced96ed332f
guitarbien <guitarbien@gmail.com>
2017-08-20 12:59:47 +0800

    改為只產生一個檔案即可

--- index.js
+++ index.js
@@ -23,14 +23,6 @@ exports.handler = function(event, context, callback){
         },
         Outputs: [
             {
-                Key: outputKey + '-1080p' + '.mp4',
-                PresetId: '1351620000001-000001' //Generic 1080p
-            },
-            {
-                Key: outputKey + '-720p' + '.mp4',
-                PresetId: '1351620000001-000010' //Generic 720p
-            },
-            {
                 Key: outputKey + '-web-720p' + '.mp4',
                 PresetId: '1351620000001-100070' //Web Friendly 720p
             }
