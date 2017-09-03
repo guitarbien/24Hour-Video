324a46f9fe721401496b30465f2acf675616a869
guitarbien <guitarbien@gmail.com>
2017-09-03 22:58:50 +0800

    套用 Auth0 實作

---
+++ website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'guitarbien.auth0.com',
+        clientId: 'DZQasLSC7lGxtct6mqPLjfqdJ6pYJGM9'
+    },
+    apiBaseUrl: 'https://API-GATEWAY-URL/dev'
+};
