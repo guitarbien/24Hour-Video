85b136c7720220d3d82c83d97267541e4f81a227
guitarbien <guitarbien@gmail.com>
2017-09-11 23:20:08 +0800

    更新 video list

--- website/js/main.js
+++ website/js/main.js
@@ -1,13 +1,6 @@
-/**
- * Created by Peter Sbarski
- * Serverless Architectures on AWS
- * http://book.acloud.guru/
- * Last Updated: Feb 11, 2017
- */
-
-
 (function(){
-    $(document).ready(function(){
-        userController.init(configConstants);
-    });
+	$(document).ready(function(){
+		userController.init(configConstants);
+		videoController.init(configConstants);
+	});
 }());
